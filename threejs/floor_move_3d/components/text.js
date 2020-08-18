51712fd50c451638267104130f2b1dc1da15ccbe
Hasan Gazi Karaşahin <hasan.karasahin@olleco.com>
2020-08-19 01:27:57 +0300

    floor+move+3d++

--- threejs/floor_move_3d/components/text.js
+++ threejs/floor_move_3d/components/text.js
@@ -4,14 +4,14 @@ function add2dText(props){
 	// create a canvas element
 	var canvas1 = document.createElement('canvas');
 	var context1 = canvas1.getContext('2d');
-	context1.font = "Bold 40px Arial";
+	context1.font = "Bold 30px Arial";
 	context1.fillStyle = "rgba(255,0,0,0.95)";
     context1.fillText(props.text, 0, 70);
-    
+
 	// canvas contents will be used for a texture
-	var texture1 = new THREE.Texture(canvas1) 
+	var texture1 = new THREE.Texture(canvas1)
 	texture1.needsUpdate = true;
-      
+
     var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
     material1.transparent = true;
 
@@ -19,8 +19,9 @@ function add2dText(props){
         new THREE.PlaneGeometry(canvas1.width, canvas1.height),
         material1
       );
-	mesh1.position.set(0,60,0);
+	mesh1.position.set(0,60,20);
+	mesh1.rotation.set(0,20,0);
 	scene.add( mesh1 );
-	
+
 	/////// draw image on canvas /////////
 }
